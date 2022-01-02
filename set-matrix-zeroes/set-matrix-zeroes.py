2665449f87381ffdd0a61fa8c180048b673b4453
Anchal_Sharma <71165891+Anchals24@users.noreply.github.com>
2022-01-02 17:57:40 +0530

    Time: 174 ms (14.26%), Space: 15 MB (74.44%) - LeetHub

--- set-matrix-zeroes/set-matrix-zeroes.py
+++ set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,18 +1,18 @@
-import copy
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        mat = copy.deepcopy(matrix)
-        for i in range(len(mat)):
-            for j in range(len(mat[i])):
-                if mat[i][j] == 0:
-                    for a in range(len(matrix[i])):
-                        matrix[i][a] = 0
-                    for b in range(len(matrix)):
-                        matrix[b][j] = 0
-        #print(matrix)
-        
-                        
+        rows = [0]*len(matrix) #[0,1,0]
+        columns = [0]*len(matrix[0]) #[0,1,0]
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if matrix[i][j] == 0:
+                    rows[i] = 1
+                    columns[j] = 1
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if rows[i] == 1 or columns[j] == 1:
+                    matrix[i][j] = 0
         
