2123445a504da375d3834938e5ee065f1ea76538
Anchal_Sharma <71165891+Anchals24@users.noreply.github.com>
2022-01-05 19:22:53 +0530

    Time: 36 ms (52.54%), Space: 14.3 MB (13.29%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -3,6 +3,28 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        #[2,0,2,1,1,0]
+        maxi = max(nums) #2
+        L = [0] * (maxi+1) #[0,0,0]
+        #print(L)
+        for i in nums: #i = 0
+            L[i] += 1  #[2,2,2]
+        #print(L)#L = [2,2,2]
+        j = 0 #0
+        k = 0 #0
+        while j < len(L): #3
+            if L[j] != 0: #j = 0, k = 2
+                nums[k] = j #[0,0,2,1,1,0]
+                k += 1 #2
+                L[j] -= 1  #[0,2,2]
+            else:
+                #k += 1
+                j += 1
+            
+
+            
+            
+            
+        
         
         
