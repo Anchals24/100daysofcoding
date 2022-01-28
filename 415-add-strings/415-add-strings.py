0aaa2653d8498f51c12a4759aa1cd15785077d6d
Anchal_Sharma <71165891+Anchals24@users.noreply.github.com>
2022-01-28 21:25:19 +0530

    Time: 63 ms (33.75%), Space: 13.8 MB (100.00%) - LeetHub

---
+++ 415-add-strings/415-add-strings.py
@@ -0,0 +1,7 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        num1 = int(num1)
+        num2 = int(num2)
+        ans = num1 + num2
+        return str(ans)
+        
