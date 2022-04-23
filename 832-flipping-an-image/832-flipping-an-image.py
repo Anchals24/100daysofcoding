acd7c390c7898e94e8e730e4ddd4757fbf03260e
Anchal_Sharma <71165891+Anchals24@users.noreply.github.com>
2022-04-23 21:18:49 +0530

    Time: 56 ms (76.09%), Space: 14 MB (21.67%) - LeetHub

--- 832-flipping-an-image/832-flipping-an-image.py
+++ 832-flipping-an-image/832-flipping-an-image.py
@@ -2,8 +2,21 @@ class Solution:
     def rev(self, l):
         first = 0
         last = len(l) - 1
-        while first < last:
+        while first <= last:
             l[first], l[last] = l[last], l[first]
+            if first == last and l[first] == 0:
+                l[first] = 1
+            elif first == last and l[first] == 1:
+                l[first] = 0
+            else: 
+                if l[first] == 0:
+                    l[first] = 1
+                elif l[first] == 1:
+                    l[first] = 0
+                if l[last] == 0:
+                    l[last] = 1
+                elif l[last] == 1:
+                    l[last] = 0
             first += 1
             last -= 1
         return l
@@ -12,12 +25,14 @@ class Solution:
             self.rev(i)
         #print(image)
         #[[0,1,1], [0,1,0], [1,1,1]]
+        """
         for j in range(len(image)):#3
             for k in range(len(image[j])):#3
                 if image[j][k] == 0:
                     image[j][k] = 1
                 elif image[j][k] == 1:
                     image[j][k] = 0
+        """
         return image
                 
         
