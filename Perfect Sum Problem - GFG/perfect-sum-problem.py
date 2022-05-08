1a34115818054d6f7e2d42f386b57d4e567f4662
Anchal_Sharma <71165891+Anchals24@users.noreply.github.com>
2022-05-08 09:01:44 +0530

    Added solution - LeetHub

---
+++ Perfect Sum Problem - GFG/perfect-sum-problem.py
@@ -0,0 +1,33 @@
+#User function Template for python3
+class Solution:
+	def perfectSum(self, arr, n, sum):
+		# code here
+		DP = [[0] * (sum+1) for _ in range(n+1)]
+	    for j in range(sum+1): 
+            DP[0][j] = 0
+        DP[0][0] = 1
+	    for i in range(1, n+1):
+	        for j in range(0, sum+1):
+	           if arr[i-1] > j:
+	               DP[i][j] = DP[i-1][j]
+	           else:
+	               DP[i][j] = DP[i-1][j-arr[i-1]] + DP[i-1][j]
+	    ans = DP[n][sum]
+	    return ans % 1000000007
+	                
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		n,sum = input().split()
+		n,sum = int(n),int(sum)
+		arr = [int(x) for x in input().split()]
+		ob = Solution()
+		ans = ob.perfectSum(arr,n,sum)
+		print(ans)
+
+# } Driver Code Ends
