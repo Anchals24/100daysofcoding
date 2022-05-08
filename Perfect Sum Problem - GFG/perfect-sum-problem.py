0951d9399141c3fac825e046f0bf3a335dd1b960
Anchal_Sharma <71165891+Anchals24@users.noreply.github.com>
2022-05-08 09:14:52 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.py
+++ Perfect Sum Problem - GFG/perfect-sum-problem.py
@@ -2,7 +2,14 @@
 class Solution:
 	def perfectSum(self, arr, n, sum):
 		# code here
-		DP = [[-1] * (sum+1) for row in range(n+1)]
+		#DP = [[-1] * (sum+1) for row in range(n+1)]
+		DP = []
+		for row in range(n+1):
+		    column = []
+		    for c in range(sum+1):
+		        column.append(-1)
+		    DP.append(column)
+		    
 	    for j in range(sum+1): 
             DP[0][j] = 0
         DP[0][0] = 1
