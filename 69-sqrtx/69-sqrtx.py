0194119a8d36d05361381c5d198fb5f6f5a93727
Anchal_Sharma <71165891+Anchals24@users.noreply.github.com>
2022-05-12 08:25:45 +0530

    Time: 42 ms (77.18%), Space: 13.9 MB (11.18%) - LeetHub

--- 69-sqrtx/69-sqrtx.py
+++ 69-sqrtx/69-sqrtx.py
@@ -2,20 +2,38 @@ class Solution:
     def mySqrt(self, x: int) -> int:
         #2*2 = 4
         #3*3 = 9
+        if x == 1:
+            return 1
         if x == 0:
             return 0
-        i = 1
-        prev = 1
-        j = 1
-        #x = 16
-        while True:
-            i = j * j #16
-            if i == x: 
-                return j
-            if i > x:
-                return prev
-            prev = j #3
-            j += 1 #4
+        low = 1
+        high = x-1
+        #8
+        while low <= high: #1 < 7
+            mid = (low+high)//2 #3
+            if mid * mid == x:
+                return mid
+            if mid * mid > x:
+                high = mid - 1 #3
+            if mid * mid < x:
+                low = mid + 1 #3
+        return high
+            
+            
+        
+        """
+        low = 0
+        high = len(arr)-1
+        while low < high:
+            mid = (low + high)// 2
+            if arr[mid] == target:
+                return mid
+            if arr[mid] > target:
+                high = mid - 1
+            if arr[mid] < target:
+                low = mid + 1
+        return -1
+        """
         
         
             
