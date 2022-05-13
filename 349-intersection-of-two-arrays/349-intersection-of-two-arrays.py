231f231171fbea4e92948d69722fa18688a9c5e1
Anchal_Sharma <71165891+Anchals24@users.noreply.github.com>
2022-05-13 07:04:10 +0530

    Time: 93 ms (17.93%), Space: 14 MB (68.96%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.py
@@ -1,17 +1,35 @@
 class Solution:
-    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        D = {}
-        for n in nums1:
-            if n in D:
-                D[n] += 1
+    def Binarysearch(self, arr, target):
+        low = 0
+        high = len(arr) - 1
+        while low <= high:
+            mid = (low+high)//2
+            if arr[mid] == target:
+                return True
+            elif arr[mid] > target:
+                high = mid - 1
             else:
-                D[n] = 1
-        Set = set()
-        for i in nums2:
-            if i in D and D[i] > 0:
-                Set.add(i)
-                D[i] -= 1
-        return Set
+                low = mid + 1
+        return False
+    
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        l1 = len(nums1)
+        l2 = len(nums2)
+        S = set()
+        nums1.sort()
+        nums2.sort()
+        if l1 < l2:
+            for i in nums1:
+                if self.Binarysearch(nums2, i) == True:
+                    S.add(i)
+        else:
+            for i in nums2:
+                if self.Binarysearch(nums1, i) == True:
+                    S.add(i)
+        return S
+                
+                
+        
                 
         
         
