51c030f42ffd6a0d41e8b11c4b99ca454f81689b
Anchal_Sharma <71165891+Anchals24@users.noreply.github.com>
2022-05-15 09:39:13 +0530

    Time: 48 ms (37.49%), Space: 13.9 MB (70.75%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,24 @@
+from collections import deque
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 != 0:
+            return False
+        Pairs = (('(',')') , ('[',']'), ('{','}'))
+        Open = ("[", "(", "{")
+        S = deque()
+        for i in s:
+            if i in Open:
+                S.append(i)
+            if i not in Open and len(S) == 0:
+                return False
+            if i not in Open:
+                t = S.pop()
+                if (t,i) not in Pairs:
+                    return False
+        if len(S) != 0:
+            return False
+        return True
+                    
+                
+                
+        
