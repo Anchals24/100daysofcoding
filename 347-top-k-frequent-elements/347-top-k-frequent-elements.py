8de19c42b8b5dd13ce410e52db58c6d903d61ce2
Anchal_Sharma <71165891+Anchals24@users.noreply.github.com>
2022-09-13 19:17:07 +0530

    Time: 251 ms (10.63%), Space: 18.3 MB (99.47%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,3 +1,4 @@
+import heapq as heap
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         D = {}
@@ -6,12 +7,22 @@ class Solution:
                 D[n] += 1
             else:
                 D[n] = 1
+        max_heap = []
+        S = set()
         ans = []
+        for n in range(len(nums)):
+            if nums[n] not in S:
+                heap.heappush(max_heap, (-1 * D[nums[n]], nums[n]))
+            S.add(nums[n])
         while k > 0:
-            keyMax = max(D.items(), key = operator.itemgetter(1))[0]
-            ans.append(keyMax)
-            D[keyMax] = -1
+            mini = heap.heappop(max_heap)  #(cnt, num)
+            ans.append(mini[1])
             k -= 1
         return ans
-
+            
+            
+            
+        
+            
+        
         
